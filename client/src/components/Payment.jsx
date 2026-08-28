eea5867607c05119325159d8b504e7aa851107de
HIMANSHU6666 <bhatthimanshu66@gmail.com>
2026-08-28 15:37:20 +0530

    Update Payment.jsx

--- client/src/components/Payment.jsx
+++ client/src/components/Payment.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import {useNavigate} from 'react-router-dom';
 import {useCart} from '../context/CartContext.jsx';
+import api from '../api.js';
 
 const Payment = ({ orderId }) => {
     const navigate = useNavigate();
@@ -8,20 +9,13 @@ const Payment = ({ orderId }) => {
 
     const handlePayment = async () => {
         try {
-            const response = await fetch("http://localhost:5000/api/payment/create-payment", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                credentials: "include",
-                body: JSON.stringify({
-                    orderId
-                })
+            const response = await api.post("/payment/create-payment", {
+                orderId
             });
 
-            const data = await response.json();
-
+            const data = response.data;
             console.log("Payment Data :", data);
+
             const options = {
                 key: data.key,
                 amount: data.amount * 100,
@@ -30,20 +24,14 @@ const Payment = ({ orderId }) => {
                 handler: async function (response){
                     console.log("Payment Response",response);
                     try{
-                        const verifyResponse = await fetch ("http://localhost:5000/api/payment/verify-payment",{
-                            method:"POST",
-                            headers:{
-                                "Content-Type":"application/json"
-                            },
-                            credentials:"include",
-                            body:JSON.stringify({
+                        const verifyResponse = await api.post("/payment/verify-payment",{
                                 paymentId:response.razorpay_payment_id,
                                 orderId:response.razorpay_order_id,
                                 signature:response.razorpay_signature
-                            })
+                            
                         }
                     );
-                    const data = await verifyResponse.json();
+                    const data = verifyResponse.data;
                     console.log("Verification response",data);
                     if(data.success){
                         clearCartState();
